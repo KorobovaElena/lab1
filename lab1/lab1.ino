341f7dacb12c95ba4d266240c9b50d066a59aad3
KorobovaElena <elen0603.ek@gmail.com>
2019-11-11 14:53:01 +0300

    база для задачи

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,16 +1,43 @@
 #include "button.h"
 
 #define PIN_BUTTON 5
-
 Button button(PIN_BUTTON);
 
+int lampsPins[] = {6, 7, 8};
+
+#define totalModes 4;
+int currentMode;
+
+int delayTime;
+int stepspeed;
+
 void setup() {
-  
+  currentMode = 0;
+  delayTime = 500;
 }
 
 void loop() {
-    if (button.wasPressed())
-    {
-
-    }
+  if (button.wasPressed())
+  {
+    currentMode = (currentMode + 1) % totalModes;
+  }
+  
+  switch (currentMode)
+  {
+    case 1:
+      stepspeed = 1;
+      delayTime = 500;
+    break;
+    case 2:
+      stepspeed = 5;
+      delayTime = 250;
+    break;
+    case 3:
+      stepspeed = 25;
+      delayTime = 100;
+      break;
+    default:
+    
+    break;
+  }
 }
